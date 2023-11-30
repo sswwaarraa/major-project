5a3eb761563171db40927b045c30fc368390e9be
sswwaarraa <152210581+sswwaarraa@users.noreply.github.com>
2023-11-30 16:06:15 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,13 @@
+// Main speed so it does not change throughout the program.
+let drivespeed = 60
+// Line sensor instructions
 basic.forever(function () {
-	
+    lineSensors.displaySensors()
+    if (lineSensors.checkSensor(IRSensor.LEFT, IRColour.BLACK)) {
+        motion.turnLeft(drivespeed)
+    } else if (lineSensors.checkSensor(IRSensor.RIGHT, IRColour.BLACK)) {
+        motion.turnRight(drivespeed)
+    } else if (lineSensors.checkSensor(IRSensor.MIDDLE, IRColour.BLACK)) {
+        motion.driveStraight(drivespeed)
+    }
 })
